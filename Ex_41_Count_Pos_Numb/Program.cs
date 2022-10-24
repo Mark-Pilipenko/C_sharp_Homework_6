d89bab0b9041e9a78127d0c2309761b5376ce5ed
Mark <kalmark@mail.ru>
2022-10-25 00:24:55 +0300

    Add Ex_41_Count_Pos_Numb

---
+++ Ex_41_Count_Pos_Numb/Program.cs
@@ -0,0 +1,19 @@
+﻿int mist = 0;
+string m = string.Empty;
+int count = 0;
+while (mist == 0)
+{
+    try
+    {
+        Console.Write("Введите число или s для окончания ввода: ");
+        m = Console.ReadLine();
+        if (m == "s") break;
+        int noun = Convert.ToInt32(m);
+        if (noun > 0) count++;
+    }
+    catch
+    {
+        Console.WriteLine("Ошибочный ввод!");
+    }
+}
+Console.WriteLine($"Введенных чисел больше нуля: {count}.");
